545d7d12a076a3e1e36c53c33122bf0ce1b62da5
vebbis321 <veb.dan@hotmail.com>
2024-06-12 16:54:47 +0200

    main should be in main

--- lox.py
+++ lox.py
@@ -1,14 +1,1 @@
-from pathlib import Path
-from sys import version_info, platform
-
-class Lox:
-    @staticmethod
-    def main(args):
-        if (args.len > 1):
-            sys.stdout.write("Usage: jlox [script]")
-            sys.exit(64)
-        elif (args.len == 1):
-            
-
-
 
