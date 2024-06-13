1be8d8a31d52ec491e0e24b0050414ad5d7f4872
vebbis321 <veb.dan@hotmail.com>
2024-06-13 18:04:48 +0200

    finished 4.1

--- lox/lox.py
+++ lox/lox.py
@@ -1,4 +1,4 @@
-from os import stat
+from os import lockf, stat
 import sys
 from pathlib import Path
 from dataclasses import dataclass
@@ -18,7 +18,16 @@ class Scanner:
     def __init__(self, source: str) -> None:
         pass
 
+    def scan_tokens(self) -> [Token]:
+        yes = Token("nois")
+        return [yes]
+
 class Lox:
+    # i guess whis will be static
+    # im a man of types
+    had_error: bool = False
+
+
     @staticmethod 
     def usage() -> None:
         sys.stdout.write("Usage: jlox [script]")
@@ -36,11 +45,26 @@ class Lox:
         
         Lox.run(source)
 
+        # indicate an error in the exit code
+        if Lox.had_error:
+            sys.exit(65)
+
     @staticmethod
     def run(source: str) -> None:
         scanner = Scanner(source)
         tokens = scanner.scan_tokens()
 
+        for token in tokens:
+            print(token)
+
+    @staticmethod
+    def error(line: int, message: str) -> None:
+        Lox.report(line, "", message)
+
+
+    @staticmethod
+    def report(line: int, where: str, message: str) -> None:
+        print(f'[line {line}] Error{where}: {message}')
 
     @staticmethod
     def run_prompt() -> None:
@@ -54,6 +78,7 @@ class Lox:
                 # gets the user input
                 expr = input()
 
+
                 # split by spaces an gets the first input from the array
                 first = expr.split(' ')[0]
 
@@ -64,14 +89,14 @@ class Lox:
                 else:
                     # else run dem expressions
                     Lox.run(expr)
+                    Lox.had_error = False
 
             except KeyboardInterrupt as ki:
-                print(f"KeyboardInterrupt deez nutz with err: ${ki}")
+                print(f"KeyboardInterrupt deez nutz with err: {ki}")
 
             # End of file error...I guess it's when nothing is passed
             except EOFError:
                 exit(0)
-
 
 
 
