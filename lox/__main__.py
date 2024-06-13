2b83125c1600b36de0aa5c08a4f115326d6d880b
vebbis321 <veb.dan@hotmail.com>
2024-06-13 17:20:22 +0200

    implemeted repl and no errors

--- lox/__main__.py
+++ lox/__main__.py
@@ -1,8 +1,13 @@
-import Lox
+from lox.lox import Lox
 import sys
 
 def main(args) -> None:
-    if (args.len > 1):
-        Lox.usage(64)
-    elif (args.len == 1):
-        Lox.run_file()
+    if (len(args) > 1):
+       Lox.usage(64)
+    elif (len(args) == 1):
+        Lox.run_file(args[0])
+    else:
+        Lox.run_prompt()
+
+# python is strange
+main(sys.argv[1:])
--- lox/lox.py
+++ lox/lox.py
@@ -1,26 +1,76 @@
 from os import stat
 import sys
 from pathlib import Path
+from dataclasses import dataclass
+
+COMMANDS = {
+    'exit': exit,
+    # 'credits': lox_credits,
+    # 'copyright': lox_copyright,
+    # 'license': lox_license,
+}
+
+@dataclass
+class Token:
+    token: str
+
+class Scanner:
+    def __init__(self, source: str) -> None:
+        pass
 
 class Lox:
     @staticmethod 
-    def usage():
+    def usage() -> None:
         sys.stdout.write("Usage: jlox [script]")
-        sys.exit(64)
 
+        # error: command line usage error
+        sys.exit(64)
 
     @staticmethod    
     def run_file(filename) -> None:
+        # get path of filename
         path = Path(filename).absolute()
+
+        # reads the file and closes
         source = path.read_text(encoding='utf-8', errors='strict')
         
         Lox.run(source)
 
     @staticmethod
-    def run(source) -> None:
-        pass
+    def run(source: str) -> None:
+        scanner = Scanner(source)
+        tokens = scanner.scan_tokens()
 
 
+    @staticmethod
+    def run_prompt() -> None:
+
+        # repl: read, evaluate, print, loop
+        while True:
+            try:
+                # a classic repl prompt
+                print('>>> ', end='')
+
+                # gets the user input
+                expr = input()
+
+                # split by spaces an gets the first input from the array
+                first = expr.split(' ')[0]
+
+                # command or expression
+                if first in COMMANDS:
+                    # example: if exit, exit will be run
+                    COMMANDS[first]()
+                else:
+                    # else run dem expressions
+                    Lox.run(expr)
+
+            except KeyboardInterrupt as ki:
+                print(f"KeyboardInterrupt deez nutz with err: ${ki}")
+
+            # End of file error...I guess it's when nothing is passed
+            except EOFError:
+                exit(0)
 
 
 
