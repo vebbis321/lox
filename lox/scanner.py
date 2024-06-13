16d7c4475b4ed67bc83235d16f38ab3dc0fb289b
vebbis321 <veb.dan@hotmail.com>
2024-06-13 22:00:23 +0200

    first impl of token and started on scanner

--- lox/scanner.py
+++ lox/scanner.py
@@ -1,11 +1,27 @@
-
+from typing import List, Union, Any, Optional
 
 from lox.token import Token
+from lox.token_type import TokenType
 
 class Scanner:
     def __init__(self, source: str):
-        pass
+        self.__source = source
+        self.__tokens: List[Token]
+        self.__start: int = 0
+        self.__current: int = 0
+        self.__line: int = 1
 
-    def scan_tokens(self) -> [Token]:
-        yes = Token("nois")
-        return [yes]
+    def __is_at_end(self) -> bool:
+        return self.__current >= len(self.__source)
+
+    def __scan_token(self):
+        pass
+        
+    def scan_tokens(self) -> List[Token]:
+        while not self.__is_at_end():
+            self.__start = self.__current
+            self.__scan_token()
+
+        self.__tokens.append(Token(TokenType.EOF, TokenType.EOF.value, None, self.__line))
+
+    
--- lox/token.py
+++ lox/token.py
@@ -2,4 +2,13 @@ from lox.token_type import TokenType
 
 
 class Token:
-    token: str
+
+    # what the duck is an object in py
+    def __init__(self, token_type: TokenType, lexeme: str, litteral, line: int) -> None:
+        self.token_type = token_type
+        self.lexeme = lexeme
+        self.litteral = litteral
+        self.line = line
+    
+    def __str__(self) -> str:
+        return f'{self.token_type} {self.lexeme} {self.litteral} {self.line}'
