1aa142386d135b7103a9384feb511b30432a0efb
vebbis321 <veb.dan@hotmail.com>
2024-06-13 22:26:11 +0200

    i think advance() works

--- lox/scanner.py
+++ lox/scanner.py
@@ -1,5 +1,6 @@
 from typing import List, Union, Any, Optional
 
+from lox import token_type
 from lox.token import Token
 from lox.token_type import TokenType
 
@@ -13,19 +14,53 @@ class Scanner:
 
     def __is_at_end(self) -> bool:
         return self.__current >= len(self.__source)
+    
+    def __advance(self) -> str:
+        x = self.__source[self.__current]
+        self.__current += 1
+        return x
+        
 
-    def __add_token(self, token_type: TokenType):
-        self.__add_token(token_type, None)
+    def __add_token(self, typ: TokenType):
+        self.__add_token(typ, None)
 
-    def __add_token(self, token_type: TokenType, literal: Any):
-        text = self.source[self.__start: self.__current]
-        self.__tokens.append(Token(token_type, text, literal, self.__line))
+    def __add_token(self, typ: TokenType, literal: Any):
+        text = self.__source[self.__start: self.__current]
+        self.__tokens.append(Token(typ, text, literal, self.__line))
 
     def __scan_token(self):
         c = self.__advance()
 
-    match c:
-        case '(': __add_token()
+        match c:
+            case '(': 
+                self.__add_token(TokenType.LEFT_PAREN)
+            case ')': 
+                self.__add_token(TokenType.RIGHT_PAREN)
+            case '{': 
+                self.__add_token(TokenType.LEFT_BRACE)
+            case '}': 
+                self.__add_token(TokenType.RIGHT_BRACE)
+            case ',': 
+                self.__add_token(TokenType.COMMA)
+            case '.': 
+                self.__add_token(TokenType.DOT)
+            case '-': 
+                self.__add_token(TokenType.MINUS)
+            case '+': 
+                self.__add_token(TokenType.PLUS)
+            case ';': 
+                self.__add_token(TokenType.SEMICOLON)
+            case '*': 
+                self.__add_token(TokenType.STAR)
+
+
+
+
+
+
+
+
+
 
         
     def scan_tokens(self) -> List[Token]:
