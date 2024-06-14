a5d554cdf07d29a56f230ad089306534c3906712
vebbis321 <veb.dan@hotmail.com>
2024-06-14 16:10:36 +0200

    python structs??? okay

--- lox/token.py
+++ lox/token.py
@@ -1,16 +1,16 @@
 from lox.token_type import TokenType
 
 from typing import Any
+from dataclasses import dataclass
 
-
+# I think this works
+@dataclass
 class Token:
 
-    # what the duck is an object in py
-    def __init__(self, token_type: TokenType, lexeme: str, litteral: Any, line: int) -> None:
-        self.token_type = token_type
-        self.lexeme = lexeme
-        self.litteral = litteral
-        self.line = line
+    token_type: TokenType
+    lexeme: str
+    litteral: Any
+    line: int
     
     def __str__(self) -> str:
         return f'{self.token_type} {self.lexeme} {self.litteral} {self.line}'
